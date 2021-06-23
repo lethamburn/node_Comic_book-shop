f9ec60d661c19b92f55d042acb1c3f868533a3f2
Antonio Rosales <antoniorosalesmartinez90@gmail.com>
2021-06-23 12:49:55 +0200

    Pestaña status terminada

--- routes/stats.routes.js
+++ routes/stats.routes.js
@@ -2,11 +2,22 @@ const express = require("express");
 const router = express.Router();
 const Comic = require("../models/Comic.model");
 const Manga = require("../models/Manga.model");
+const total = 0;
 
+router.get("/", async(req, res, next) => {
 
-router.get("/", (req, res, next) => {
-  return res.render("stats", { user: req.user });
+  const userlog = req.user;
+  const comics = await Comic.find({
+    idUser: userlog._id,
+  });
+  const mangas = await Manga.find({
+    idUser: userlog._id,
+  });
+
+  return res.status(200).render("stats", {
+    user: req.user,
+    arrayComics: comics,
+    arrayMangas: mangas,
+  });
 });
-
 module.exports = router;
-  
